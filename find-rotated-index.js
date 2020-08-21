ffadf629587fd3937ebe09fcc36020d867dc9d96
von Buelow <jaspar@UMICH.EDU>
2020-08-21 17:57:08 -0400

    Find rotated index

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -8,7 +8,7 @@ function findRotatedIndex(arr, num) {
     let midIdx = Math.floor((start + end) / 2);
     let midVal = arr[midIdx];
 
-    if (midVal > arr[midIdx + 1 ]) {
+    if (midVal > arr[midIdx + 1]) {
       pivot = midIdx + 1;
       start = end + 1;
     } else if (arr[start] <= midVal) {
@@ -18,6 +18,27 @@ function findRotatedIndex(arr, num) {
     }
   }
 
+  if(num > arr[arr.length -1]) {
+    return binarySearch(arr, num, 0, pivot - 1);
+  } else {
+    return binarySearch(arr, num, pivot, arr.length -1);
+  }
+}
+
+function binarySearch(arr, num, start, end) {
+  while (start <= end) {
+    // find the middle value
+    let midIdx = Math.floor((start + end) / 2);
+    let midVal = arr[midIdx];
+    if(midVal < num) {
+      start = midIdx + 1;
+    } else if (midVal > num) {
+      end = midIdx - 1;
+    } else {
+      return midIdx;
+    }
+  }
+  return -1;
 }
 
 module.exports = findRotatedIndex
