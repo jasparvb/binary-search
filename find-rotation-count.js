8710921bbb95da4e05737ce0dd959d7f0ef3cf59
von Buelow <jaspar@UMICH.EDU>
2020-08-21 18:08:15 -0400

    Find rotation count

--- find-rotation-count.js
+++ find-rotation-count.js
@@ -1,5 +1,20 @@
-function findRotationCount() {
-  
+function findRotationCount(arr) {
+  let start = 0;
+  let end = arr.length -1;
+  while (start <= end) {
+    // find the mid value
+    let midIdx = Math.floor((start + end) / 2);
+    let midVal = arr[midIdx];
+
+    if (midVal > arr[midIdx + 1]) {
+      return midIdx + 1;
+    } else if (arr[start] <= midVal) {
+      start = midIdx + 1;
+    } else {
+      end = midIdx - 1;
+    }
+  }
+  return 0;
 }
 
 module.exports = findRotationCount
